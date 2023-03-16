bc4025a7d47cfe7d144dff70c961e35476d0639e
deepak-sahu <deepak.mscis3@iiitmk.ac.in>
2023-03-16 16:39:12 +0400

    Add files via upload

---
+++ hello.ps1
@@ -0,0 +1,5 @@
+open-google-search.ps1 [<CommonParameters>]
+
+[<CommonParameters>]
+    This script supports the common parameters: Verbose, Debug, ErrorAction, ErrorVariable, WarningAction, 
+    WarningVariable, OutBuffer, PipelineVariable, and OutVariable.
